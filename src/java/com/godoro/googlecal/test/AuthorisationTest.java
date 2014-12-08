801da91577731b5db87d38de3718c1d9ffdcfac5
erolerten <erolerten@ThinkPad-T440s>
2014-12-08 21:17:23 +0200

    dokumentasyon degismis... eski dosyayi silip yeniden basladim

--- src/java/com/godoro/googlecal/test/AuthorisationTest.java
+++ src/java/com/godoro/googlecal/test/AuthorisationTest.java
@@ -5,15 +5,25 @@
  */
 package com.godoro.googlecal.test;
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeRequestUrl;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.Events;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
+import sun.net.www.protocol.http.HttpURLConnection;
 
 /**
  *
@@ -21,19 +31,66 @@ import java.security.GeneralSecurityException;
  */
 public class AuthorisationTest {
 
-    public void setUp() throws IOException, GeneralSecurityException {
-        HttpTransport httpTransport=GoogleNetHttpTransport.newTrustedTransport();
-        JacksonFactory jsonFactory=JacksonFactory.getDefaultInstance();
-        
+    public static void main(String[] args) {
+        try {
+            setUp();
 
-        String clientId = "310094007194-mlp0aq7cqk7nauqhpomtu9ikuuicf5g6.apps.googleusercontent.com";
-        String clientSecret = "tyPn3v610xkkY-dK_8GfPURu";
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
 
-        String redirectUrl = "urn:ietf:wg:oauth:2.0:oob";
+        } catch (GeneralSecurityException g) {
+            System.out.println(g.getMessage());
+        }
+
+    }
+
+    public static void setUp() throws IOException, GeneralSecurityException {
+        HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        JacksonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+
+        String clientId = "310094007194-8vjru4juqokls3cvdqq8bv2kj4k7mqqa.apps.googleusercontent.com";
+        String clientSecret = "BzapxG21JmAL0erEHtxNsMHp";
+
+        String redirectUrl = "http://localhost";
         String scope = "https://www.googleapis.com/auth/calendar";
-        
 
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow(httpTransport, jsonFactory, clientId, clientSecret,
+                Collections.singleton(scope));
+        String authorizationUrl = flow.newAuthorizationUrl()
+                .setRedirectUri(redirectUrl).build();
+
+        System.out.println("Go to following link on your browser");
+        System.out.println(authorizationUrl);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("what is the authorization code?");
+        String code = in.readLine();
+
+        GoogleTokenResponse reponse = flow.newTokenRequest(code)
+                .setRedirectUri(redirectUrl).execute();
+
+        Credential credential = new GoogleCredential.Builder()
+                .setTransport(httpTransport).setJsonFactory(jsonFactory)
+                .setClientSecrets(clientId, clientSecret).build()
+                .setFromTokenResponse(reponse);
+
+        Calendar service = new Calendar.Builder(httpTransport, jsonFactory, credential)
+                .setApplicationName(authorizationUrl)
+                .setApplicationName("My first Project").build();
+
+        String pageToken = null;
         
+        do {
+            Events events = service.events().list(clientId).setPageToken(pageToken).execute();
+            
+                    
+                    
+            List<Event> items = events.getItems();
+            for (Event event : items) {
+                System.out.println(event.getSummary());
+            }
+            pageToken = events.getNextPageToken();
+        } while (pageToken != null);
 
     }
 
